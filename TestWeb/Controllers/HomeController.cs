1017f9c422ce75206e6d0591da9a65bc95e7ec28
yarmdy <jxy2000121@sina.com>
2024-09-02 10:33:28 +0800

    加入注释

--- TestWeb/Controllers/HomeController.cs
+++ TestWeb/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace TestWeb.Controllers
         }
         public IActionResult Index()
         {
+            //这里可以看到，延迟Factory模式不使用依然要创建Factory对象，而代理模式绝不会创建任何对象,只在使用的那一刻创建
             return Json(new { Code=1 ,Lazy= LazyClasses,Lazy2 = _delayClass.Service });
         }
         public override string? ToString()
