7ef659d8632cacf0fe076240e95d54720280bfd9
yarmdy <jxy2000121@sina.com>
2024-09-14 11:16:33 +0800

    加入测试环节

--- TestWeb/Controllers/HomeController.cs
+++ TestWeb/Controllers/HomeController.cs
@@ -16,20 +16,29 @@ namespace TestWeb.Controllers
         }
         public IActionResult Index()
         {
-
-            var test = ActivatorUtilities.CreateInstance<TestInject>(Services, new DefaultLazyClass { Name = "新的", Age = 100, Balance = -1000 });
-
-
             //这里可以看到，延迟Factory模式不使用依然要创建Factory对象，而代理模式绝不会创建任何对象,只在使用的那一刻创建
-            return Json(new { Code=1 ,Lazy= LazyClasses,Lazy2 = _delayClass.Service,Test=test });
+            return Json(new { Code=1 ,Lazy= LazyClasses,Lazy2 = _delayClass.Service });
         }
         public override string? ToString()
         {
             return "I'm HomeController Has Index Action!";
         }
+        [NonAction]
+        public void Test()
+        {
+            // 传统实例化
+            TestInject test1 = new TestInject(_delayClass.Service, Services);
+            // 反射
+            TestInject test2 = (TestInject)Activator.CreateInstance(typeof(TestInject), _delayClass.Service, Services)!;
+
+            // 依赖反射
+            TestInject test3 = ActivatorUtilities.CreateInstance<TestInject>(Services);
+            // 依赖反射提供一个参数
+            TestInject test4 = ActivatorUtilities.CreateInstance<TestInject>(Services, _delayClass.Service);
+            // 依赖反射提供二个参数
+            TestInject test5 = ActivatorUtilities.CreateInstance<TestInject>(Services, _delayClass.Service, Services);
+        }
     }
-
-
     public class TestInject
     {
         public ILazyClass LazyClass { get; set; }
